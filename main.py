3f23d8b5af4d67ef051145179a29ff6b552fbea6
yliCoesio <77597102+YuJ-Li@users.noreply.github.com>
2023-09-30 22:27:51 -0400

    starter method
    starter method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Read date from unigram_date
-def read_unigram_data(file_path="data/unigram_counts"):
+# Read data from unigram_counts
+def read_unigram_data(file_path="./data/unigram_counts.txt"):
     unigram_model = {}
     with open(file_path, 'r') as file:
         for line in file:
@@ -9,6 +9,46 @@ def read_unigram_data(file_path="data/unigram_counts"):
     return unigram_model
 
 
+# Read data from bigram_counts
+def read_bigram_data(file_path="./data/bigram_counts.txt"):
+    bigram_model = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip().split()
+            index_i, index_j, prob = int(line[0]), int(line[1]), float(line[2])
+            if index_i not in bigram_model:
+                bigram_model[index_i] = {}
+            bigram_model[index_i][index_j] = prob
+    return bigram_model
+
+
+# Read data from trigram_counts
+def read_trigram_data(file_path="./data/trigram_counts.txt"):
+    trigram_model = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip().split()
+            index_i, index_j, index_k, prob = int(line[0]), int(line[1]), int(line[2]), float(line[3])
+            if index_i not in trigram_model:
+                trigram_model[index_i] = {}
+            if index_j not in trigram_model[index_i]:
+                trigram_model[index_i][index_j] = {}
+            trigram_model[index_i][index_j][index_k] = prob
+    return trigram_model
+
+
+# Read data from vocab
+def read_vocab_data(file_path="./data/vocab.txt"):
+    vocab = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip().split()
+            index, word = int(line[0]), line[1]
+            vocab[index] = word
+    return vocab
+
+
 if __name__ == '__main__':
-    a = read_unigram_data("data/unigram_counts")
-    print(a)
+    a = read_unigram_data()
+    b = read_bigram_data()
+    print(b[1])
